055caee7c92f793008a160b40d1a89fead9d8e3a
yunlew531 <yunlew531@gmail.com>
2021-04-24 10:17:19 +0800

    puzzle game

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const { onMounted, ref, reactive, watch } = Vue;
+const app = {
+  setup() {
+    const puzzleOriginArr = reactive({ imgArr: [] });
+    const puzzleCompleteArr = reactive({ imgArr: [] });
+    const correctNum = ref(0);
+    const isComplete = ref(false);
+
+    const moveComplete = (img, index) => {
+      puzzleOriginArr.imgArr.splice(index, 1);
+      puzzleCompleteArr.imgArr.push(img);
+    };
+
+    const moveOrigin = (img, index) => {
+      puzzleCompleteArr.imgArr.splice(index, 1);
+      puzzleOriginArr.imgArr.push(img);
+    };
+
+    watch(() => puzzleCompleteArr.imgArr, (newArr) => {
+      correctNum.value = 0;
+      newArr.forEach((img, key) => {
+        if (img.index === key) {
+          correctNum.value++;
+        }
+      });
+    }, { deep: true });
+
+    watch(correctNum, () => {
+      console.log(correctNum.value);
+      if (correctNum.value === 9) {
+        isComplete.value = true;
+      }
+    })
+
+    onMounted(() => {
+      const api = '../json/puzzle.json';
+      axios.get(api)
+        .then(res => {
+          puzzleOriginArr.imgArr = res.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
+    return {
+      puzzleOriginArr,
+      puzzleCompleteArr,
+      moveComplete,
+      moveOrigin,
+      isComplete,
+    };
+  },
+};
+
+Vue.createApp(app).mount('#app');
